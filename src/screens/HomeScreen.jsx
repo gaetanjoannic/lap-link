f685c68bf6181ad11071005e1849733c25138de3
Audrey Norvez <audrey.norvez@gmail.com>
2025-04-25 00:49:32 +0200

    add Icon, Splash screen and Home view

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
